5d649ec70adcfec7965a5efcc1567c9341b13e0b
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-05-25 17:51:56 +0200

    Aggiungere i file di progetto.

---
+++ BBSplugins/Minesweeper.cs
@@ -0,0 +1,66 @@
+﻿// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft BBS
+// 
+// CasaSoft BBS is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft BBS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft BBS.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+using Casasoft.BBS.Interfaces;
+using Casasoft.BBS.UI;
+
+namespace Casasoft.BBS.Plugins
+{
+    class Minesweeper : TextScreenBase
+    {
+        #region constructors
+        private const string defaultText = "@Minesweeper";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="c">Client reference</param>
+        /// <param name="s">Server reference</param>
+        public Minesweeper(IBBSClient c, IServer s) : this(c, s, defaultText) { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="c">Client reference</param>
+        /// <param name="s">Server reference</param>
+        /// <param name="prev">Link to caller screen</param>
+        public Minesweeper(IBBSClient c, IServer s, IScreen prev) : this(c, s, defaultText, prev) { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="c">Client reference</param>
+        /// <param name="s">Server reference</param>
+        /// <param name="text">Text to parse and optional parameters separated by semicolon</param>
+        public Minesweeper(IBBSClient c, IServer s, string text) : this(c, s, text, null) { }
+
+        /// <summary>
+        /// Complete constructor
+        /// </summary>
+        /// <param name="c">Client reference</param>
+        /// <param name="s">Server reference</param>
+        /// <param name="text">Text to parse and optional parameters separated by semicolon</param>
+        /// <param name="prev">Link to caller screen</param>
+        public Minesweeper(IBBSClient c, IServer s, string text, IScreen prev) : base(c, s, text, prev)
+        {
+        }
+        #endregion    
+    }
+}
